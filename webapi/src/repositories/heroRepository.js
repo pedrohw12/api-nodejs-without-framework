28b50b1c3c2f62632bf0aaa0e86a6f5e67bf06f4
Pedro Wanderley <peu-zico@hotmail.com>
2021-02-17 21:32:57 -0300

    first commit

---
+++ webapi/src/repositories/heroRepository.js
@@ -0,0 +1,28 @@
+const { readFile, writeFile } = require('fs').promises;
+
+class HeroRepository {
+  constructor({ file }) {
+    this.file = file;
+  }
+
+  async _currentFileContent() {
+    return JSON.parse(await readFile(this.file));
+  }
+
+  async find(itemId) {
+    const all = await this._currentFileContent();
+    if (!itemId) return all;
+
+    return all.find(({ id }) => itemId === id);
+  }
+}
+
+module.exports = HeroRepository;
+
+const heroRepository = new HeroRepository({
+  file: "../../database/data.json",
+});
+heroRepository
+  .find()
+  .then(console.log)
+  .catch((error) => console.log(error));
